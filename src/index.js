81538785ca7599380a2ad5fd388d8f1fabc50491
Ada <107205214+Senpaiada@users.noreply.github.com>
2022-06-16 11:57:42 +0100

    Done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 // complete the function
-function prime (num) {
-  // code goes here
-}
+function prime (num) { 
+  //edge case
+  if(num < 0 || num ===1){
+    return false
+  } else {
+    for (let i = 2; i < num; i++){
+       if (num % i === 0){
+         return false
+       }
+    }
+  }
+   return true
+}  
+  console.log(prime(1))
+
 
 function solution (arg) {
   return prime(arg);
